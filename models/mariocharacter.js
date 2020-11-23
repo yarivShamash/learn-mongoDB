4f50e81bbaab6e3fe8fd3754da2132726d40cc0a
Yariv Shamash <yarivshamash@Yarivs-MacBook-Pro.local>
2020-11-23 18:31:42 +0200

    first mocha test

--- models/mariocharacter.js
+++ models/mariocharacter.js
@@ -2,18 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create a Schema for models to follow
-
 const MarioCharacterScheme = new Schema({
   name: String,
   vweight: Number,
 });
 
 // Create a Model and assign a schema for it
-
 const MarioCharacter = mongoose.model("mariocharacter", MarioCharacterScheme);
 // In the line above "mariocharcter" is the collection name also called the module name this model is based on the MaricCharacterSchema schema
 //Note: Only characters based on the mariocharcter module will be stored in the mariocharacter collection
 
 // Finally export the module
-
 module.export = MarioCharacter;
